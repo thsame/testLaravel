19123dd85d7aaa508a356f1a585b72577263cbd3
thsame <thsame@icloud.com>
2019-07-06 13:21:30 +0900

    文章追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
-<h1>テスト中</h1>
 
+<h1>自己紹介</h1>
+<p>４０歳無職、就職活動中です。情報系の学科卒です。IT業界未経験ですが拾っていただける企業を探しています。</br>
+  構文だけならC言語、C++、JAVA、PHP、Ruby、Pythonなど理解できます。<br>
+  前職では、VBAでマクロをひたすら組んでました。Action Scriptで教材を作成してたこともあります</br>
+  現在はLaravelを学習中です。このサイトもLaravelで作っています。</br>
+  Railsとかもやりたいなぁと思う今日この頃。時間だけはたっぷりあるはず。</br>
+</p>
+
+<img src="books.jpg"  alt="test">
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Test</title>
+    <title>Portfolio</title>
     @include('style-sheet')
   </head>
     <body>
