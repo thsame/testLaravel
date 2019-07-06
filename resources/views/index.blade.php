d01a08b6c69539ce22c07855a94a4e2702cfa212
thsame <thsame@icloud.com>
2019-07-06 21:49:51 +0900

    ポートフォリオ

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,25 @@
 @extends('layout')
 
 @section('content')
+<div class="jumbotron jumbotron-extend">
+  <div class="container-fluid jumbotron-container">
+    <span class = "display-1 mb-20">Portfolio</span>
+    <br>
+    <br>
+    <p>４０歳でIT業界にキャリアチェンジしたい。</p>
+    <p>という野望を持ちつつ、会社辞めちゃった無職おじさんのサイト<p/>
+    <p><a class="btn btn-black" href="#" role="button">CONTACT ME</a></p>
+  </div>
+</div>
 
+<!-- img src="{{ asset('img/books.jpg') }}" class = "w-5 img-fluid center-block" -->
 
 <h1>自己紹介</h1>
 <p>４０歳無職、就職活動中です。情報系の学科卒です。IT業界未経験ですが拾っていただける企業を探しています。</br>
   構文だけならC言語、C++、JAVA、PHP、Ruby、Pythonなど理解できます。<br>
   前職では、VBAでマクロをひたすら組んでました。Action Scriptで教材を作成してたこともあります</br>
   現在はLaravelを学習中です。このサイトもLaravelで作っています。</br>
-  Railsとかもやりたいなぁと思う今日この頃。時間だけはたっぷりあるはず。</br>
-</p>
+  Railsとかもやりたいなぁと思う今日この頃。</br>
 
-<img src="books.jpg"  alt="test">
+</p>
 @endsection
--- resources/views/style-sheet.blade.php
+++ resources/views/style-sheet.blade.php
@@ -1,3 +1,30 @@
 <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'>
-<style>body {padding-top: 80px;}</style>
+<style>body {padding-top: 80px;}
+  .jumbotron-extend {
+    position: relative;
+  height: 100vh;
+  min-height: 300px;
+  background: url({{ asset('img/books.jpg') }}) no-repeat center center;
+  background-size: cover;
+  }
+  .jumbotron-container {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+}
+.site-name {
+    margin-bottom: 40px;
+    font-family: 'Playfair Display', serif;
+}
+.btn-black {
+    border-radius: 0;
+    background-color: #000;
+    color: #fff;
+    font-family: 'Avenir', serif;
+}
+.btn-black:hover {
+     background-color: #fff;
+     color: #000;
+}
+</style>
